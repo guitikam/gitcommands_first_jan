81954beef01df39d50d573af926cb0e407bb909d
guitikam <geethikamuralidharan@gmail.com>
2022-08-03 11:36:10 +0100

    added some hashes

--- gitcommands.py
+++ gitcommands.py
@@ -5,7 +5,7 @@ git commit -m "Changes message"
 git log
 git checkout *******The code in the log*********
 git checkout master    ###(to revert back)
-git remote add ***insert branch name***  #####copy and paste link from github new repository######
+git remote add ***insert branch name***  #####copy and paste link from github new repository########
 git push -u ***** insert branch name***** master
 git remote -v ((This will give you te name of the branch. trial in this case))
 
